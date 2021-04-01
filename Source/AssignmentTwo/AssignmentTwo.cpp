41c275efd8316006c5096a8582634afdff91e068
KielanT <34215034+KielanT@users.noreply.github.com>
2021-04-01 11:45:03 +0100

    Initial commit

---
+++ Source/AssignmentTwo/AssignmentTwo.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AssignmentTwo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AssignmentTwo, "AssignmentTwo" );
---
+++ Source/AssignmentTwo/AssignmentTwoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AssignmentTwoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASSIGNMENTTWO_API AAssignmentTwoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
